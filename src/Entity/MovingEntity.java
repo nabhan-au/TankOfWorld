4439340e940108de6b1643a79374d3aed9da8da0
Siratee K <sirateek@gmail.com>
2022-05-02 17:34:41 +0700

    Add special type of entity that can animate

---
+++ src/Entity/MovingEntity.java
@@ -0,0 +1,14 @@
+public abstract class MovingEntity extends Entity {
+    private int dx;
+    private int dy;
+
+    public MovingEntity(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void animate() {
+        this.setPosition(this.getX() + dx, this.getY() + dy);
+    }
+
+}
