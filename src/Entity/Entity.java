361eb8cb385e549203929ec08aa0f405f38bf048
Siratee K <sirateek@gmail.com>
2022-05-02 17:34:59 +0700

    Declare a package in the Entity

--- src/Entity/Entity.java
+++ src/Entity/Entity.java
@@ -1,3 +1,5 @@
+package Entity;
+
 public abstract class Entity {
     private int x;
     private int y;
--- src/Entity/MovingEntity.java
+++ src/Entity/MovingEntity.java
@@ -1,3 +1,5 @@
+package Entity;
+
 public abstract class MovingEntity extends Entity {
     private int dx;
     private int dy;
