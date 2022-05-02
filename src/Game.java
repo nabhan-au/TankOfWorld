e71c7b6af9ec04947b81c82bbf9172742806c461
Siratee K <sirateek@gmail.com>
2022-05-02 18:04:18 +0700

    Add the Main Game Object for starting the game

---
+++ src/Game.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+import GUI.GameUI;
+
+import java.awt.*;
+
+public class Game extends JFrame {
+    public static void main(String[] args) throws Exception {
+        GameUI gameUI = new GameUI();
+    }
+}
