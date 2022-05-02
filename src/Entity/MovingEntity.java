7279342f92bb983a56097688afd1e24be57d8ef7
Siratee K <sirateek@gmail.com>
2022-05-02 17:50:54 +0700

    Add the setMovingDirection into the Moving Entiry and create another constructore for setting the dx,dy

--- src/Entity/MovingEntity.java
+++ src/Entity/MovingEntity.java
@@ -5,7 +5,18 @@ public abstract class MovingEntity extends Entity {
     private int dy;
 
     public MovingEntity(int x, int y) {
+        this(x, y, 0, 0);
+    }
+
+    public MovingEntity(int x, int y, int dx, int dy) {
         super(x, y);
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public void setMovingDirection(int dx, int dy) {
+        this.dx = dx;
+        this.dy = dy;
     }
 
     @Override
