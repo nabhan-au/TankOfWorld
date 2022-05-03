7dec6f9993d9c2b6a5083ee97b806d4e07ab4880
Siratee K <sirateek@gmail.com>
2022-05-03 14:01:07 +0700

    Update the MovingEntity to instead keep the Directio nand the speed of the entity

--- src/Entity/MovingEntity.java
+++ src/Entity/MovingEntity.java
@@ -1,27 +1,42 @@
 package Entity;
 
 public abstract class MovingEntity extends Entity {
-    private int dx;
-    private int dy;
+    private Direction direction;
+    private int speed;
+    private boolean isMoving = false;
 
     public MovingEntity(int x, int y) {
-        this(x, y, 0, 0);
+        this(x, y, 1);
+        this.direction = Direction.UP;
     }
 
-    public MovingEntity(int x, int y, int dx, int dy) {
+    public MovingEntity(int x, int y, int speed) {
         super(x, y);
-        this.dx = dx;
-        this.dy = dy;
+        this.speed = speed;
     }
 
-    public void setMovingDirection(int dx, int dy) {
-        this.dx = dx;
-        this.dy = dy;
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public Direction getDirection() {
+        return this.direction;
+    }
+
+    public void move() {
+        this.isMoving = true;
+    }
+
+    public void stop() {
+        this.isMoving = false;
     }
 
     @Override
     public void animate() {
-        this.setPosition(this.getX() + dx, this.getY() + dy);
+        if (!isMoving) {
+            return;
+        }
+        this.setPosition(this.getX() + (speed * direction.getX()), this.getY() + (speed * direction.getY()));
     }
 
 }
