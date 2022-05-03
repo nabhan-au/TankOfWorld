60485e2de47a734eafb9310f2920641d5bfb1ce2
Siratee K <sirateek@gmail.com>
2022-05-03 14:42:07 +0700

    Create the ImageSetBuilder and add it into the ImageSet base class, Apply the change to the tankImageSet

--- src/Entity/ImageSet/TankImageSet.java
+++ src/Entity/ImageSet/TankImageSet.java
@@ -2,57 +2,54 @@ package Entity.ImageSet;
 
 import java.awt.Image;
 
-import javax.swing.ImageIcon;
-
 import Entity.Direction;
 
 public class TankImageSet extends ImageSet {
-    private Image up;
-    private Image down;
-    private Image left;
-    private Image right;
+    public TankImageSet(ImageSetBuilder builder) {
+        super(builder);
+    }
 
-    /**
-     * This is a class containing the set of image for a tank
-     * 
-     * @param up    Path to the file for up direction.
-     * @param left  Path to the file for down direction.
-     * @param right Path to the file for rigth direction.
-     * @param down  Path to the file for down direction.
-     */
-    public TankImageSet(String up, String left, String right, String down) {
-        super(up, left, right, down);
-        this.up = new ImageIcon(up).getImage();
-        this.down = new ImageIcon(down).getImage();
-        this.left = new ImageIcon(left).getImage();
-        this.right = new ImageIcon(right).getImage();
+    public static class TankImageBuilder extends ImageSetBuilder {
+
+        @Override
+        public TankImageSet build() {
+            return new TankImageSet(this);
+        }
+
     }
 
     @Override
     public Image getImage(Direction direction) {
         switch (direction) {
             case DOWN:
-                return this.down;
+                return this.getDown();
             case LEFT:
-                return this.left;
+                return this.getLeft();
             case RIGHT:
-                return this.right;
+                return this.getRight();
             case UP:
-                return this.up;
+                return this.getUp();
             default:
-                return this.up;
+                return this.getUp();
 
         }
 
     }
 
-    public static TankImageSet getTankImageSet(int tankNo) {
-        switch (tankNo) {
-            case 0:
-                return new TankImageSet("assets/imgs/tank/tank_a_up.png",
-                        "assets/imgs/tank/tank_a_left.png",
-                        "assets/imgs/tank/tank_a_right.png",
-                        "assets/imgs/tank/tank_a_down.png");
+    public static enum TankImage {
+        A;
+    }
+
+    public static TankImageSet getTankImageSet(TankImage tankImageNo) {
+        TankImageBuilder tankImageBuilder = new TankImageBuilder();
+        switch (tankImageNo) {
+            case A:
+                tankImageBuilder
+                        .up("assets/imgs/tank/tank_a_up.png")
+                        .left("assets/imgs/tank/tank_a_left.png")
+                        .right("assets/imgs/tank/tank_a_right.png")
+                        .down("assets/imgs/tank/tank_a_down.png");
+                return tankImageBuilder.build();
             default:
                 throw new Error("Unknown Tank Number.");
         }
