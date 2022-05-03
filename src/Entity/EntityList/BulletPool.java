ab54842aad7f85ca3e11b736b8def2a1309671c7
Siratee K <sirateek@gmail.com>
2022-05-03 16:44:41 +0700

    Create an bullet pool to manage the bullet object

---
+++ src/Entity/EntityList/BulletPool.java
@@ -0,0 +1,35 @@
+package Entity.EntityList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BulletPool {
+    private static BulletPool bulletPoolInstance;
+    private List<Bullet> bulletPool = new ArrayList<Bullet>();
+
+    private BulletPool() {
+        for (int i = 0; i < 50; i++) {
+            bulletPool.add(new Bullet());
+        }
+    }
+
+    public Bullet borrowBullet() {
+        if (bulletPool.size() <= 0) {
+            throw new Error("Not enough bullet");
+        }
+        return bulletPool.remove(0);
+    }
+
+    public void returnBullet(Bullet bullet) {
+        bullet.setPosition(-1, -1);
+        bullet.stop();
+        this.bulletPool.add(bullet);
+    }
+
+    public static BulletPool getInstance() {
+        if (bulletPoolInstance == null) {
+            bulletPoolInstance = new BulletPool();
+        }
+        return bulletPoolInstance;
+    }
+}
