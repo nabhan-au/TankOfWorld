229fc7b69bd463cf85474d11660a9318b41d4de6
Siratee K <sirateek@gmail.com>
2022-05-03 14:02:11 +0700

    Update the KeyHandler to set the direction by using the Enumerate instead

--- src/KeyHandler.java
+++ src/KeyHandler.java
@@ -1,3 +1,4 @@
+import Entity.Direction;
 import Entity.Tank;
 
 import java.awt.event.KeyAdapter;
@@ -9,36 +10,38 @@ public class KeyHandler extends KeyAdapter {
     private int moveRight;
     private int moveUp;
     private int moveDown;
+    private int shoot;
 
-    protected KeyHandler(Tank tank, int moveLeft, int moveRight, int moveUp, int moveDown) {
+    protected KeyHandler(Tank tank, int moveLeft, int moveRight, int moveUp, int moveDown, int shoot) {
         this.tank = tank;
         this.moveLeft = moveLeft;
         this.moveRight = moveRight;
         this.moveDown = moveDown;
         this.moveUp = moveUp;
+        this.shoot = shoot;
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode() == moveUp) {
-            tank.setDirection("up");
-            tank.moveUp();
+        if (e.getKeyCode() == moveUp) {
+            tank.setDirection(Direction.UP);
         } else if (e.getKeyCode() == moveDown) {
-            tank.setDirection("down");
-            tank.moveDown();
+            tank.setDirection(Direction.DOWN);
         } else if (e.getKeyCode() == moveLeft) {
-            tank.setDirection("left");
-            tank.moveLeft();
+            tank.setDirection(Direction.LEFT);
         } else if (e.getKeyCode() == moveRight) {
-            tank.setDirection("right");
-            tank.moveRight();
+            tank.setDirection(Direction.RIGHT);
+        } else if (e.getKeyCode() == shoot) {
+            // TODO: Implement the shoot logic.
         }
+        tank.move();
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        if(e.getKeyCode() == moveUp || e.getKeyCode() == moveDown || e.getKeyCode() == moveLeft || e.getKeyCode() == moveRight) {
-            tank.setMovingDirection(0, 0);
+        if (e.getKeyCode() == moveUp || e.getKeyCode() == moveDown || e.getKeyCode() == moveLeft
+                || e.getKeyCode() == moveRight) {
+            tank.stop();
         }
     }
 }
