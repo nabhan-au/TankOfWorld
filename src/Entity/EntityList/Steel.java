fd87bdc9b1c2d25e58323e9aadfc34e8cd7e35b8
Siratee K <sirateek@gmail.com>
2022-05-13 17:11:37 +0700

    Remove unused code.

--- src/Entity/EntityList/Steel.java
+++ src/Entity/EntityList/Steel.java
@@ -3,7 +3,7 @@ package Entity.EntityList;
 import Entity.Entity;
 
 public class Steel extends Entity {
-    ;
+
     public Steel(int x, int y, int width, int height) {
         super(x, y);
         setSize(width, height);
