632afb3cfa5b7877c63fbb1054673dc814981219
Siratee K <sirateek@gmail.com>
2022-05-14 15:12:07 +0700

    Remove the Game Panal and apply the game layer into the Game class

--- src/Main/Game.java
+++ src/Main/Game.java
@@ -1,10 +1,8 @@
 package Main;
 
 import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import Entity.*;
 import Entity.EntityList.Brick;
@@ -17,11 +15,10 @@ import Presentation.InvisibleBlockUIObject;
 import Presentation.SteelUIObject;
 import Presentation.TankUIObject;
 import Presentation.TreeUIObject;
-import Presentation.UIObject;
-import Presentation.ImageSet.BlockImageSet;
-import Presentation.ImageSet.ImageSet;
 import Presentation.ImageSet.TankImageSet;
 import Presentation.ImageSet.TankImageSet.TankImage;
+import Presentation.Layers.FloorGameLayer;
+import Presentation.Layers.GameLayer;
 
 import java.awt.*;
 
@@ -34,8 +31,11 @@ public class Game extends JFrame implements PropertyChangeListener {
     public static int BOARD_SIZE = 1000;
     public static int BLOCK_SIZE = 40;
     private Thread gameThread;
-    private GamePanal gamePanal;
     private Boolean gameOver = false;
+    private JPanel floorGameLayer = new FloorGameLayer();
+    private GameLayer tankGameLayer = new GameLayer();
+    private GameLayer blockGameLayer = new GameLayer();
+    private GameLayer effectGameLayer = new GameLayer();
 
     public Game() {
         this.map = new Map(BOARD_SIZE, BOARD_SIZE, 2, this);
@@ -43,10 +43,49 @@ public class Game extends JFrame implements PropertyChangeListener {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setPreferredSize(new Dimension(BOARD_SIZE, BOARD_SIZE));
 
-        this.gamePanal = new GamePanal(map);
-        add(gamePanal);
+        // TODO: Temporary add the Tank Creator in the GamePanal.
+        tankGameLayer.addUIObject(new TankUIObject(map.getTank(0), TankImageSet.getTankImageSet(TankImage.A), this));
+        tankGameLayer.addUIObject(new TankUIObject(map.getTank(1), TankImageSet.getTankImageSet(TankImage.B), this));
+
+        // TODO: Temporary generate UIObject based on the type.
+        for (Entity entity : map.getEntities()) {
+
+            if (entity instanceof Brick) {
+                blockGameLayer.addUIObject(new BrickUIObject(entity));
+            } else if (entity instanceof Tree) {
+                blockGameLayer.addUIObject(new TreeUIObject(entity));
+            } else if (entity instanceof Steel) {
+                blockGameLayer.addUIObject(new SteelUIObject(entity));
+            } else if (entity instanceof InvisibleBlock) {
+                blockGameLayer.addUIObject(new InvisibleBlockUIObject(entity));
+            }
+        }
+
+        JLayeredPane jLayeredPane = getLayeredPane();
+
+        jLayeredPane.add(effectGameLayer);
+        jLayeredPane.add(tankGameLayer);
+        jLayeredPane.add(blockGameLayer);
+        jLayeredPane.add(floorGameLayer);
         pack();
-        gamePanal.requestFocus();
+
+        addKeyListener(new KeyHandler(
+                map.getTank(0),
+                KeyEvent.VK_LEFT,
+                KeyEvent.VK_RIGHT,
+                KeyEvent.VK_UP,
+                KeyEvent.VK_DOWN,
+                KeyEvent.VK_CONTROL));
+
+        addKeyListener(new KeyHandler(
+                map.getTank(1),
+                KeyEvent.VK_A,
+                KeyEvent.VK_D,
+                KeyEvent.VK_W,
+                KeyEvent.VK_S,
+                KeyEvent.VK_SPACE));
+
+        requestFocus();
 
     }
 
@@ -61,7 +100,7 @@ public class Game extends JFrame implements PropertyChangeListener {
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    gamePanal.repaint();
+                    repaint();
                 }
             }
         };
@@ -78,79 +117,23 @@ public class Game extends JFrame implements PropertyChangeListener {
     }
 
     @Override
+    public void repaint() {
+        super.repaint();
+        floorGameLayer.repaint();
+        tankGameLayer.repaint();
+        effectGameLayer.repaint();
+        blockGameLayer.repaint();
+    }
+
+    @Override
     public void propertyChange(PropertyChangeEvent event) {
         if (event.getPropertyName() == DomainEvent.GameOver.toString()) {
             this.gameOver();
         }
     }
 
-    class GamePanal extends JPanel {
-        private List<UIObject> uiObjects = new ArrayList<UIObject>();
-        private ImageSet floorImageSet = BlockImageSet.getBlockImage(BlockImageSet.BlockImage.Floor);
-
-        public GamePanal(Map map) {
-            super();
-            // TODO: Temporary add the Tank Creator in the GamePanal.
-            uiObjects.add(new TankUIObject(map.getTank(0), TankImageSet.getTankImageSet(TankImage.A)));
-            uiObjects.add(new TankUIObject(map.getTank(1), TankImageSet.getTankImageSet(TankImage.B)));
-
-            // TODO: Temporary generate UIObject based on the type.
-            for (Entity entity : map.getEntities()) {
-
-                if (entity instanceof Brick) {
-                    uiObjects.add(new BrickUIObject(entity));
-                } else if (entity instanceof Tree) {
-                    uiObjects.add(new TreeUIObject(entity));
-                } else if (entity instanceof Steel) {
-                    uiObjects.add(new SteelUIObject(entity));
-                } else if (entity instanceof InvisibleBlock) {
-                    uiObjects.add(new InvisibleBlockUIObject(entity));
-                }
-            }
-
-            addKeyListener(new KeyHandler(
-                    map.getTank(0),
-                    KeyEvent.VK_LEFT,
-                    KeyEvent.VK_RIGHT,
-                    KeyEvent.VK_UP,
-                    KeyEvent.VK_DOWN,
-                    KeyEvent.VK_CONTROL));
-
-            addKeyListener(new KeyHandler(
-                    map.getTank(1),
-                    KeyEvent.VK_A,
-                    KeyEvent.VK_D,
-                    KeyEvent.VK_W,
-                    KeyEvent.VK_S,
-                    KeyEvent.VK_SPACE));
-        }
-
-        @Override
-        public void paint(Graphics g) {
-            super.paint(g);
-            paintFloor(g);
-            paintUIObjects(g);
-        }
-
-        public void paintUIObjects(Graphics g) {
-            for (int i = uiObjects.size() - 1; i > -1; i--) {
-                UIObject uiObject = uiObjects.get(i);
-                if (uiObject.getIsRemovable()) {
-                    uiObjects.remove(i);
-                } else {
-                    uiObject.paint(g);
-                }
-            }
-        }
-
-        public void paintFloor(Graphics g) {
-            for (int i = 0; i < BOARD_SIZE; i += BLOCK_SIZE) {
-                for (int j = 0; j < BOARD_SIZE; j += BLOCK_SIZE) {
-                    g.drawImage(floorImageSet.getUp(), i, j, BLOCK_SIZE, BLOCK_SIZE, null);
-                }
-            }
-        }
-
+    public GameLayer getEffectGameLayer() {
+        return this.effectGameLayer;
     }
 
     public static void main(String[] args) throws Exception {
