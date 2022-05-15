0ed8ec2c2d77f14f86141a96df8e140fcc6efa05
Siratee K <sirateek@gmail.com>
2022-05-15 13:06:13 +0700

    Adsd the messagebox option selector in menu

--- src/Main/GameState/Menu.java
+++ src/Main/GameState/Menu.java
@@ -1,13 +1,16 @@
 package Main.GameState;
 
 import Main.Game;
-import Main.GameState.State;
+import Main.MapData;
 import Presentation.Layers.GameLayer;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Menu extends State {
     private Game stateOwner;
@@ -55,7 +58,7 @@ public class Menu extends State {
             g.drawString(text, x, y);
         }
 
-        public int getXforCenterText(Graphics g,String text, Font font, int x, int width) {
+        public int getXforCenterText(Graphics g, String text, Font font, int x, int width) {
             FontMetrics metrics = g.getFontMetrics(font);
             int size = metrics.stringWidth(text);
             return x + (width - size) / 2;
@@ -66,8 +69,60 @@ public class Menu extends State {
         @Override
         public void keyPressed(KeyEvent e) {
             if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                File files = new File("maps/");
+                String[] fileLists = files.list();
+                List<MapData> mapList = new ArrayList<MapData>();
+                for (String fileName : fileLists) {
+                    try {
+                        mapList.add(new MapData("maps/" + fileName));
+                    } catch (Exception exception) {
+                        System.out.println(exception);
+                        continue;
+                    } catch (Error error) {
+                        System.out.println(error);
+                        continue;
+                    }
+                }
+
+                List<String> options = new ArrayList<String>();
+                for (MapData mapData : mapList) {
+                    options.add(mapData.getMapName() + " (" + mapData.getMapSize() + "x"
+                            + mapData.getMapSize() + ")");
+                }
+
+                int pickOptionNum = pickOptionInputDialogBox("Enter map number you want to play", options);
+                stateOwner.setMap(mapList.get(pickOptionNum - 1));
+                stateOwner.start();
                 stateOwner.setState(new TwoPlayerMode(stateOwner));
+
             }
         }
+    }
+
+    public String showInputDialogBox(String message) {
+        return JOptionPane.showInputDialog(message);
+    }
+
+    public int pickOptionInputDialogBox(String message, List<String> options) {
+        message += "\n";
+        int count = 1;
+        for (String option : options) {
+            message += count + ")" + option + "\n";
+            count++;
+        }
+        int selectOptionInt = 0;
+        while (true) {
+            String selectOption = showInputDialogBox(message);
+            try {
+                selectOptionInt = Integer.parseInt(selectOption);
+            } catch (NumberFormatException e) {
+                selectOptionInt = -1;
+            }
+            if (selectOptionInt >= 1 && selectOptionInt <= options.size()) {
+                break;
+            }
+            JOptionPane.showMessageDialog(null, "Invalid Input, Please enter option again.");
+        }
+        return selectOptionInt;
     }
 }
